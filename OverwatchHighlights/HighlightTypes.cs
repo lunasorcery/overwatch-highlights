55db4ab1d54542f8a71ed2444a5a76379e593cf5
Naomi <naomichan@users.noreply.github.com>
2017-07-30 15:49:51 +0100

    HighlightInfo: unknownUnlock is Highlight Type (#2)

---
+++ OverwatchHighlights/HighlightTypes.cs
@@ -0,0 +1,11 @@
+﻿namespace OverwatchHighlights
+{
+    public enum HighlightType : uint
+    {
+        None = 0x00000000,
+        Shutdown = 0x00000001,
+        Sharpshooter = 0x00000002,
+        Blank = 0x00000003,
+        Lifesaver = 0x00000004
+    }
+}
